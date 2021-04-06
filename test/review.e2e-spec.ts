816f548e0f15a1c81813fb6d0fe0d31156bf347b
Dima <wushu_dima@mail.ru>
2021-04-06 20:08:32 +0200

    add validation pipe for review controller/dto

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -13,8 +13,8 @@ const testDto: CreateReviewDto = {
   title: 'Heading',
   description: 'Description',
   rating: 5,
-  productId
-}
+  productId,
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -34,9 +34,20 @@ describe('AppController (e2e)', () => {
       .post('/review/create')
       .send(testDto)
       .expect(201)
-      .then(({body}: request.Response) => {
+      .then(({ body }: request.Response) => {
         createdId = body._id;
         expect(createdId).toBeDefined();
+        done();
+      });
+  });
+
+  it('/review/create (POST) - fail', (done) => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send({ ...testDto, rating: 0 })
+      .expect(400)
+      .then(({ body }: request.Response) => {
+        console.log(body);
         done();
       });
   });
@@ -45,7 +56,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/review/byProduct/' + productId)
       .expect(200)
-      .then(({body}: request.Response) => {
+      .then(({ body }: request.Response) => {
         expect(body.length).toBe(1);
         done();
       });
@@ -55,7 +66,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/review/byProduct/' + new Types.ObjectId().toHexString())
       .expect(200)
-      .then(({body}: request.Response) => {
+      .then(({ body }: request.Response) => {
         expect(body.length).toBe(0);
         done();
       });
@@ -72,12 +83,11 @@ describe('AppController (e2e)', () => {
       .delete('/review/' + new Types.ObjectId().toHexString())
       .expect(404, {
         statusCode: 404,
-        message:REVIEW_NOT_FOUND
+        message: REVIEW_NOT_FOUND,
       });
   });
 
   afterAll(() => {
     disconnect();
   });
-
 });
