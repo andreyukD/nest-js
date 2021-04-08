4e99ea620df33a5d3644f706615432d262faec2e
Dima <wushu_dima@mail.ru>
2021-04-08 21:40:05 +0200

    add aggregation to find products by category, custom idValidationPipe, custom functions in aggregation

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -11,6 +11,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { IdValidationPipe } from 'src/pipes/ad-validation.pipe';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserEmail } from '../decorators/user-email.decorator';
 import { CreateReviewDto } from './dto/create-review.dto';
@@ -29,7 +30,7 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedDoc = await this.reviewService.delete(id);
     if (!deletedDoc) {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
@@ -39,7 +40,7 @@ export class ReviewController {
   // @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
   async byProduct(
-    @Param('productId') productId: string,
+    @Param('productId', IdValidationPipe) productId: string,
     @UserEmail() email: string,
   ) {
     // console.log(email);
