4a480b2600a06a8526f1628d9f83cd0f6f77564f
Dima <wushu_dima@mail.ru>
2021-04-09 19:50:53 +0200

    create top-page

---
+++ src/top-page/top-page.constants.ts
@@ -0,0 +1,1 @@
+export const NOT_FOUND_TOP_PAGE_ERROR = 'Not found top page with that id';
---
+++ src/top-page/top-page.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TopLevelCategory, TopPageModel } from './top-page.model';
+
+@Injectable()
+export class TopPageService {
+  constructor(
+    @InjectModel(TopPageModel)
+    private readonly topPageModel: ModelType<TopPageModel>,
+  ) {}
+
+  async create(dto: CreateTopPageDto) {
+    return this.topPageModel.create(dto);
+  }
+
+  async findById(id: string) {
+    return this.topPageModel.findById(id).exec();
+  }
+
+  async findByAlias(alias: string) {
+    return this.topPageModel.findOne({ alias: alias }).exec(); // alias as slug (unique)
+  }
+
+  async findByCategory(firstCategory: TopLevelCategory) {
+    return this.topPageModel
+      .find(
+        { firstCategory: firstCategory },
+        { alias: 1, secondCategory: 1, title: 1 }, // return only this fields
+      )
+      .exec();
+  }
+
+  async delete(id: string) {
+    return this.topPageModel.findByIdAndDelete(id).exec();
+  }
+
+  async updateById(id: string, dto: CreateTopPageDto) {
+    return this.topPageModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+  }
+}
